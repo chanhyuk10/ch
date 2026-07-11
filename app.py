0d0cb14ed5a156468aae09c76c4451dbdbc56268
chanhyuk10 <chungchanhyuk@gmail.com>
2026-07-11 16:11:40 +0900

    Fix markdown formatting for current count display

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if 'count' not in st.session_state:
     st.session_state.count = 0
     if st.button("증가"):
     count = count+1
-st.markdown(f"##현재 숫자: '(st.session_state.count)'")
+st.markdown(f"##현재 숫자: '{st.session_state.count}'")
